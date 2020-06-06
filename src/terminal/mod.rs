8225d6353dee0b1557b5d364a221df9c199ed972
Alican <aerdogan07@gmail.com>
2020-06-06 15:45:09 +0200

    Formatted code

--- src/terminal/mod.rs
+++ src/terminal/mod.rs
@@ -5,8 +5,8 @@ pub mod password;
 pub mod select;
 
 pub fn clear_all() {
-  let term = Term::stdout();
-  match term.clear_screen() {
-    _ => {}
-  }
+    let term = Term::stdout();
+    match term.clear_screen() {
+        _ => {}
+    }
 }
--- src/terminal/password.rs
+++ src/terminal/password.rs
@@ -1,12 +1,12 @@
 use dialoguer::{theme::ColorfulTheme, PasswordInput};
 
 pub fn ask_password(prompt: &str) -> Option<String> {
-  let password_opt = PasswordInput::with_theme(&ColorfulTheme::default())
-    .with_prompt(prompt)
-    .interact();
+    let password_opt = PasswordInput::with_theme(&ColorfulTheme::default())
+        .with_prompt(prompt)
+        .interact();
 
-  match password_opt {
-    Ok(password) => Some(password),
-    Err(_) => None,
-  }
+    match password_opt {
+        Ok(password) => Some(password),
+        Err(_) => None,
+    }
 }
